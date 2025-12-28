24ad3128da51085d1ea6e4f5bef8cda41517af1e
NavineDevs <navinedevs@proton.me>
2025-12-29 03:04:34 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "navine-word-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((c) => c.put(event.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => cached))
+  );
+});
